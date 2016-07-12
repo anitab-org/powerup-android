f9bbffae1c4f0a276cd68f1534ed9a8a98393a31
aanchal07 <aanchal070495@gmail.com>
2016-07-13 00:57:14 +0530

    UI changes in DressingRoom1 to sync with iOS

--- PowerUp/app/src/main/java/powerup/systers/com/datamodel/SessionHistory.java
+++ PowerUp/app/src/main/java/powerup/systers/com/datamodel/SessionHistory.java
@@ -5,9 +5,9 @@ public class SessionHistory {
     public static int currSessionID = 1;
     public static int totalPoints = 0;
     public static int currScenePoints = 0;
-    public static int eyesTotalNo = 3;
+    public static int eyesTotalNo = 9;
     public static int clothTotalNo = 9;
     public static int hairTotalNo = 11;
-    public static int faceTotalNo = 3;
+    public static int faceTotalNo = 8;
     public static int accessoryTotalNo = 14;
 }
