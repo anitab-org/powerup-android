a8789a93277b2ae3dec585fc34935a6ec6d43ed1
codingblazer <sasachin04@gmail.com>
2017-08-18 15:42:59 +0530

    Adds Splash Screen using themes

---
+++ PowerUp/app/src/main/java/powerup/systers/com/SplashActivity.java
@@ -0,0 +1,23 @@
+package powerup.systers.com;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, StartActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.animator.fade_in, R.animator.fade_out);
+                finish();
+            }
+        },1000);
+    }
+}
