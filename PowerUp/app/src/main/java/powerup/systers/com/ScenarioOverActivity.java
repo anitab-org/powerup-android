aef2df4b97046d726c37935360bea009d4bbba31
Rimjhim Bhadani <rimjhimbhadani@gmail.com>
2018-02-20 19:08:13 +0530

    Update ScenarioOverActivity.java

--- PowerUp/app/src/main/java/powerup/systers/com/ScenarioOverActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/ScenarioOverActivity.java
@@ -19,7 +19,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
