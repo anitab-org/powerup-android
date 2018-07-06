fe3672efb395b6a38e184973313b242c68efe194
Rimjhim28 <rimjhimbhdani@gmail.com>
2018-07-06 22:23:30 +0530

    Add end screens to memory match

--- PowerUp/app/src/main/java/powerup/systers/com/memory_match_game/MemoryMatchEndActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/memory_match_game/MemoryMatchEndActivity.java
@@ -1,13 +1,16 @@
 package powerup.systers.com.memory_match_game;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import powerup.systers.com.R;
+import powerup.systers.com.StartActivity;
 
 public class MemoryMatchEndActivity extends Activity {
 
@@ -17,11 +20,32 @@ public class MemoryMatchEndActivity extends Activity {
     public TextView txtMemoryCorrect;
     @BindView(R.id.txt_memory_wrong)
     public TextView txtMemoryWrong;
+    public int score, correct, wrong;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_match_end);
         ButterKnife.bind(this);
+        getValues();
+        updateViews();
+    }
+
+    @OnClick(R.id.continue_btn_memory)
+    public void clickContinue(){
+        startActivity(new Intent(MemoryMatchEndActivity.this, StartActivity.class));
+        overridePendingTransition(R.animator.fade_in_custom, R.animator.fade_out_custom);
+    }
+
+    public void updateViews(){
+        txtScore.setText(""+score);
+        txtMemoryCorrect.setText(""+correct);
+        txtMemoryWrong.setText(""+wrong);
+    }
+
+    public void getValues(){
+        score = getIntent().getIntExtra("score",0);
+        correct = getIntent().getIntExtra("correct",0);
+        wrong = getIntent().getIntExtra("wrong",0);
     }
 }
