723988806d72747cf958059f7819f7a7b94becbb
Rimjhim28 <rimjhimbhdani@gmail.com>
2018-07-24 20:41:39 +0530

    Added all the constants in SessionHistory

--- PowerUp/app/src/main/java/powerup/systers/com/data/SessionHistory.java
+++ PowerUp/app/src/main/java/powerup/systers/com/data/SessionHistory.java
@@ -2,7 +2,7 @@
  * @desc sets up the default avatar features and scenario points upon beginning the app.
  */
 
-package powerup.systers.com.data;
+package powerup.systers.com.datamodel;
 
 public class SessionHistory {
     public static int currQID = 1;
@@ -10,9 +10,9 @@ public class SessionHistory {
     public static int prevSessionID = -1;
     public static int totalPoints = 0;
     public static int currScenePoints = 0;
-    public static int eyesTotalNo = 9;
-    public static int clothTotalNo = 4;
-    public static int hairTotalNo = 16;
+    public static int eyesTotalNo = 8;
+    public static int clothTotalNo = 49;
+    public static int hairTotalNo = 17;
     public static int skinTotalNo = 16;
     public static int glassesTotalNo = 3;
     public static int bagTotalNo = 3;
@@ -23,5 +23,22 @@ public class SessionHistory {
     public static boolean sceneSchoolIsReplayed = false;
     public static boolean sceneHospitalIsReplayed = false;
     public static boolean sceneLibraryIsReplayed = false;
+    public static boolean sceneHomeLevel2IsReplayed = false;
+    public static boolean sceneSchoolLevel2IsReplayed = false;
+    public static boolean sceneHospitalLevel2IsReplayed = false;
+    public static boolean sceneLibraryLevel2IsReplayed = false;
     public static boolean hasPreviouslyCustomized = false;
+    public static int[] npcList;
+    public static boolean adult1Chosen = false;
+    public static boolean adult2Chosen = false;
+    public static boolean child1Chosen = false;
+    public static boolean child2Chosen = false;
+    public static boolean characterChosen = false;
+    public static int[] npcFullViewList;
+    public static int characterType = 0;
+    public static int selectedAdult1 = 0;
+    public static int selectedAdult2 = 0;
+    public static int selectedChild1 = 0;
+    public static int selectedChild2 = 0;
+    public static int selectedValue = 0;
 }
