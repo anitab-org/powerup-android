45ae2249a78fa0e84f05cd6aa6ff37ac6f5edcac
Rimjhim28 <rimjhimbhdani@gmail.com>
2018-07-24 23:09:32 +0530

    Rectified the import statements in SinkSwim game

--- PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimGame.java
+++ PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimGame.java
@@ -20,10 +20,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import powerup.systers.com.powerup.PowerUpUtils;
 import powerup.systers.com.ui.map_screen.MapActivity;
 import powerup.systers.com.R;
 import powerup.systers.com.data.SessionHistory;
-import powerup.systers.com.utils.PowerUpUtils;
+
 
 /**
  * Created by sachinaggarwal on 7/07/17.
